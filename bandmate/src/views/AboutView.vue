f4faa5cca26ec0effddb41f1ec0ad0009e309c1e
logan-wood <logan.loganwood@gmail.com>
2024-10-12 15:00:24 +1100

    Frontend Init
    Initialised Vue.js frontend. First commit!

---
+++ bandmate/src/views/AboutView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>BandMate Page</h1>
+    <p>A web application allowing bands to find members, and people to find bands. Created because I didn't see a good version of this from my 5 minutes of research</p>
+    <a href="/">Home</a>
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ bandmate/src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>BandMate Page</h1>
+        <a href="about">About</a>
+    </div>
+</template>
